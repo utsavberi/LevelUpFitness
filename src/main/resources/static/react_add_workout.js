cea0f5a0bd7fd123e85740aea4b1239f7c539986
uberi <uberi@wayfair.com>
2020-03-26 01:45:22 -0400

    add workout on ui

--- src/main/resources/static/react_add_workout.js
+++ src/main/resources/static/react_add_workout.js
@@ -1,4 +1,3 @@
-
 function ticks() {
     return (new Date()).getTime();
 }
@@ -12,12 +11,94 @@ class App extends React.Component {
     render() {
         return (
             <div className={"container"}>
-                <WorkoutContainer workout={jsondata}/>
+                <AddWorkoutForm/>
             </div>
         )
     }
 }
 
+class AddWorkoutForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            exercises: [],
+            numberOfExercises: 0
+        }
+    }
+
+    addExercise = () => {
+        this.setState((prevState) => ({
+            numberOfExercises: prevState.numberOfExercises + 1,
+            exercises: prevState.exercises.concat({
+                exerciseName: prevState.exerciseName,
+                sets: prevState.sets,
+                reps: prevState.reps,
+                rest: prevState.rest
+            })
+        }));
+    };
+
+    setExerciseName = (value) => {
+        this.setState(() => ({exerciseName: value}));
+    };
+
+    setSets = (value) => {
+        this.setState(() => ({sets: parseInt(value, 10)}));
+    };
+
+    setReps = (value) => {
+        this.setState(() => ({reps: parseInt(value, 10)}));
+    };
+
+    setRest = (value) => {
+        this.setState(() => ({rest: parseInt(value, 10)}));
+    };
+
+    render() {
+        return (
+            <div>
+                <input placeholder={'Workout Name'}/>
+                {this.state.exercises.map((e, i) => (
+                    <Exercise
+                        exerciseName={e.exerciseName}
+                        sets={e.sets}
+                        reps={e.reps}
+                        rest={e.rest}
+                    />))}
+                <input placeholder={'Exercise Name'} onChange={(e) => {
+                    this.setExerciseName(e.target.value)
+                }}/>
+                <input type="number" placeholder={'Sets'} onChange={(e) => {
+                    this.setSets(e.target.value)
+                }}/>
+                <input type="number" placeholder={'Reps'} onChange={(e) => {
+                    this.setReps(e.target.value)
+                }}/>
+                <input type="number" placeholder={'Rest'} onChange={(e) => {
+                    this.setRest(e.target.value)
+                }}/>
+                <button onClick={() => {
+                    this.addExercise()
+                }}>+
+                </button>
+            </div>)
+    }
+
+
+}
+
+class Exercise extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (<div>
+            {this.props.exerciseName} {this.props.set} {this.props.reps} {this.props.interval}
+        </div>)
+    }
+}
 
 const domContainer =
     document.querySelector('#add-workout-react-container');
