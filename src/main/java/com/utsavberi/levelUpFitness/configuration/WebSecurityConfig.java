f0196e0e1c29d2fd753c68d4ad78cd8eab4a69d4
uberi <uberi@wayfair.com>
2020-04-09 03:52:16 -0400

    added styling to login page

--- src/main/java/com/utsavberi/levelUpFitness/configuration/WebSecurityConfig.java
+++ src/main/java/com/utsavberi/levelUpFitness/configuration/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/public/**").permitAll()
+                .antMatchers("/public/**", "/api/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
